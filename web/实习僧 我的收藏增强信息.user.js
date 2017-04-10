d531845784578a532f60137c150420349a930c6c
hujimiya <hujimiya@outlook.com>
2017-04-11 00:31:28 +0800

    现在可以机动选择某一时刻最多可以进行多少次AJAX

--- web/实习僧 我的收藏增强信息.user.js
+++ web/实习僧 我的收藏增强信息.user.js
@@ -24,32 +24,47 @@ jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
                         hrefs[index] = element.querySelector("a").getAttribute("href");
                     });
                     var index = 0;
-                    var length = hrefs.length;
+                    var hrefsLength = hrefs.length;
                     const PRICE_SELECTOR = "span.daymoney";//待遇
                     const COMPANYNAME_SELECTOR = ".jb_det_right_top>p:first-of-type>a";//公司名
                     const PLACE_SELECTOR = "span.city";//城市名 取title即可
+                    const X_ON_TIME = 5;//同一时刻最多可以同时进行的AJAX数目
                     var loadXhr = function loadXhr(index) {
-                        var ajax = jQuery.ajax(hrefs[index]);
-                        ajax.success(function ajaxSuc(responseText, textStatus, jqXHR) {
-                            var jhtml = jQuery(responseText);
-                            interns.eq(index)
-                                .append(jhtml.find(PLACE_SELECTOR))
-                                .append(jhtml.find(PRICE_SELECTOR))
-                                .append(jhtml.find(COMPANYNAME_SELECTOR))
-                                .css("height", "auto")
-                        }).error(function ajaxErr(jqXHR, textStatus, errorString) {
-                            console.info("[Hujimiya Error]", index, hrefs[index], errorString);
-                        }).then(function ajaxThen() {
-                            // console.info(index++);
-                            // debugger;
-                            index++;
-                            console.count("[Hujimiya Count] Ajax获取职位信息");
-                            if (index < length) {
-                                loadXhr(index);
-                            } else {
-                                console.info("[Hujimiya Info]", "Complete")
-                            }
-                        });
+                        var endIndex = Math.round(Math.min(hrefsLength - 1, index + X_ON_TIME - 1));
+                        var subLength = endIndex - index + 1;
+                        var countAjaxFinished = 0;
+                        for (var i = index; i <= endIndex; i++) {
+                            (function (i) {
+                                var ajax = jQuery.ajax(hrefs[i]);
+
+                                ajax.success(function ajaxSuc(responseText, textStatus, jqXHR) {
+                                    var jhtml = jQuery(responseText);
+                                    interns.eq(i)
+                                        .append(jhtml.find(PLACE_SELECTOR))
+                                        .append(jhtml.find(PRICE_SELECTOR))
+                                        .append(jhtml.find(COMPANYNAME_SELECTOR))
+                                        .css("height", "auto")
+                                }).error(function ajaxErr(jqXHR, textStatus, errorString) {
+                                    console.info("[Hujimiya Error]", i, hrefs[i], errorString);
+                                }).then(function ajaxThen() {
+                                    // console.info(index++);
+                                    // debugger;
+                                    countAjaxFinished++;
+                                    console.count("[Hujimiya Count] Ajax获取职位信息<<"+index);
+                                    if (countAjaxFinished === subLength) {
+                                        var nextIndex = index + X_ON_TIME;
+                                        if (nextIndex < hrefsLength) {
+                                            loadXhr(nextIndex);
+                                        } else {
+                                            console.info("[Hujimiya Info]", "Complete")
+                                        }
+                                    }
+
+                                });
+
+                            })(i);
+                        }
+
 
                     };
                     loadXhr(0);
