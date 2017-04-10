b46288c1307b89d044d338c4499c6f2112547928
hujimiya <hujimiya@outlook.com>
2017-04-11 00:51:03 +0800

    增加一个timer看用时

--- web/实习僧 我的收藏增强信息.user.js
+++ web/实习僧 我的收藏增强信息.user.js
@@ -8,6 +8,20 @@
 
 console.info("[Hujimiya Info]", "实习僧go");
 // console.info(jQuery("body"));
+const timer_date = [];
+var timer_flag   = false;
+
+jQuery(window).bind("timer.go",function (event) {
+    if(!timer_flag){
+        timer_flag=!timer_flag;
+        timer_date[0] = new Date();
+    }
+});
+jQuery(window).bind("timer.end",function (event) {
+    timer_date[1] = new Date();
+    console.info("[Hujimiya Timer]",timer_date[0],"to",timer_date[1]);
+
+});
 jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
     // console.info(event.target);
     if (event.target.classList[0] === "fun_model") {
@@ -27,7 +41,7 @@ jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
                     const PRICE_SELECTOR = "span.daymoney";//待遇
                     const COMPANYNAME_SELECTOR = ".jb_det_right_top>p:first-of-type>a";//公司名
                     const PLACE_SELECTOR = "span.city";//城市名 取title即可
-                    const X_ON_TIME_STEP = 25;//同一时刻最多可以同时进行的AJAX数目
+                    const X_ON_TIME_STEP = 45;//同一时刻最多可以同时进行的AJAX数目
                     const loadXhr = function loadXhr(startIndex) {
                         const endIndex = Math.round(Math.min(hrefsLength - 1, startIndex + X_ON_TIME_STEP - 1));
                         const subLength = endIndex - startIndex + 1;
@@ -35,6 +49,7 @@ jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
                         for (var i = startIndex; i <= endIndex; i++) {
                             (function (i) {
                                 const ajax = jQuery.ajax(hrefs[i]);
+                                jQuery(window).trigger("timer.go");
                                 ajax.success(function ajaxSuc(responseText, textStatus, jqXHR) {
                                     const jhtml = jQuery(responseText);
                                     interns.eq(i)
@@ -55,6 +70,7 @@ jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
                                             loadXhr(nextIndex);
                                         } else {
                                             console.info("[Hujimiya Info]", "Complete")
+                                            jQuery(window).trigger("timer.end");
                                         }
                                     }
                                 });
