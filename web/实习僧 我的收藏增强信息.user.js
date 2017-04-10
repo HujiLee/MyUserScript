6e9ba149b0be35fe24778e74270826b9ca654d7e
hujimiya <hujimiya@outlook.com>
2017-04-11 00:06:42 +0800

    fix bug.上一个push并不能达到自动点击'职位'后显示职位的效果

--- web/实习僧 我的收藏增强信息.user.js
+++ web/实习僧 我的收藏增强信息.user.js
@@ -6,51 +6,64 @@
 // @run-at      document-end
 // ==/UserScript==
 
-console.info("shixiseng");
+console.info("[Hujimiya Info]","实习僧go");
 // console.info(jQuery("body"));
-jQuery("body").bind("DOMNodeInserted.hujimiya",function onInserted(event) {
+jQuery("body").bind("DOMNodeInserted.hujimiya", function bodyOnInserted(event) {
+    console.info(event.target)
+    if (event.target.classList[0] === "fun_model") {
+        // debugger;
+        jQuery(event.target).bind("DOMNodeInserted.hujimiya", function modelOnInserted(event) {
 
-   if(event.target.classList[0]==="fun_model"){
-       // debugger;
-       var interns = jQuery(".fun_model>div[data-stype=intern]");
-       // console.info(interns);
-       var hrefs = [];
-       interns.each(function (index, element) {
-           hrefs[index] = element.querySelector("a").getAttribute("href");
-       });
-       var index = 0;
-       var length = hrefs.length;
-       const PRICE_SELECTOR = "span.daymoney";//待遇
-       const COMPANYNAME_SELECTOR = ".jb_det_right_top>p:first-of-type>a";//公司名
-       const PLACE_SELECTOR = "span.city";//城市名 取title即可
-       var loadXhr = function (index) {
-           var ajax = jQuery.ajax(hrefs[index]);
-           ajax.success(function (responseText,textStatus,jqXHR) {
-               var jhtml = jQuery(responseText);
-               interns.eq(index)
-                   .append(jhtml.find(PLACE_SELECTOR))
-                   .append(jhtml.find(PRICE_SELECTOR))
-                   .append(jhtml.find(COMPANYNAME_SELECTOR))
-                   .css("height","auto")
-           }).error(function (jqXHR,textStatus,errorString) {
-               console.info("[Hujimiya Error]",index,hrefs[index],errorString);
-           }).then(function () {
-              console.info(index++);
-              // debugger;
-              if(index<length){
-                  loadXhr(index);
-              }else{
-                  console.info("[Hujimiya Info]Complete")
-              }
-           });
+            if (event.target.tagName == "SCRIPT") {
+                // debugger;
+                {
+                    var interns = jQuery(".fun_model>div[data-stype=intern]");
+                    // console.info(interns);
+                    var hrefs = [];
+                    interns.each(function pushToHrefs(index, element) {
+                        hrefs[index] = element.querySelector("a").getAttribute("href");
+                    });
+                    var index = 0;
+                    var length = hrefs.length;
+                    const PRICE_SELECTOR = "span.daymoney";//待遇
+                    const COMPANYNAME_SELECTOR = ".jb_det_right_top>p:first-of-type>a";//公司名
+                    const PLACE_SELECTOR = "span.city";//城市名 取title即可
+                    var loadXhr = function loadXhr(index) {
+                        var ajax = jQuery.ajax(hrefs[index]);
+                        ajax.success(function ajaxSuc(responseText, textStatus, jqXHR) {
+                            var jhtml = jQuery(responseText);
+                            interns.eq(index)
+                                .append(jhtml.find(PLACE_SELECTOR))
+                                .append(jhtml.find(PRICE_SELECTOR))
+                                .append(jhtml.find(COMPANYNAME_SELECTOR))
+                                .css("height", "auto")
+                        }).error(function ajaxErr(jqXHR, textStatus, errorString) {
+                            console.info("[Hujimiya Error]", index, hrefs[index], errorString);
+                        }).then(function ajaxThen() {
+                            // console.info(index++);
+                            // debugger;
+                            index++;
+                            console.count("[Hujimiya Count] Ajax获取职位信息");
+                            if (index < length) {
+                                loadXhr(index);
+                            } else {
+                                console.info("[Hujimiya Info]","Complete")
+                            }
+                        });
 
-       };
-       loadXhr(0);
+                    };
+                    loadXhr(0);
+                }
+                setTimeout(function clicTab() {
+                    jQuery(".back_nav li:nth-of-type(2)").click();//点击"职位"使得默认就在职位tab上
+                }, 1000);
+                jQuery(event.currentTarget).unbind("DOMNodeInserted.hujimiya");
 
+            }
 
-       jQuery(".back_nav li:nth-of-type(2)").click();//点击"职位"使得默认就在职位tab上
-       jQuery(event.currentTarget).unbind("DOMNodeInserted.hujimiya");
-   }
+        });
+        jQuery(event.currentTarget).unbind("DOMNodeInserted.hujimiya");
+    }
 
 });
 
